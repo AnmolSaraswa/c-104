b7c9140f8044814aacc04a0f2e287ee92373027d
AnmolSaraswa <113250050+AnmolSaraswa@users.noreply.github.com>
2023-02-04 07:51:39 -0800

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,38 @@
+ var y = Math.floor(Math.random()*10+1);
+
+ var x  = document.getElementById("GuessField").value;
+
+ if (x==y)
+ {
+
+    alert("CONGRATULATIONS!!!"+playername+" YOU GUESSED IT RIGHT IN"+guess+"GUESS");
+
+    guess=1;
+
+ }
+
+ else if(x>y)
+ {
+
+    guess++;
+
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+
+ }
+
+ else (x<y)
+ {
+    
+    guess++;
+
+    alert("OOPS SORRY!! TRY A GREATER NUMBER");
+
+ }
+
+
+ function playAgain()
+ {
+
+    y = Math.floor(Math.random()*10+1);
+
+ }
